c86d8d79aa081219920cf4596f6167d8dbe358d8
aolu11 <aolu11@qq.com>
2015-08-06 11:08:36 +0800

    high serverity log not append to low severity

--- glog.go
+++ glog.go
@@ -694,13 +694,10 @@ func (l *loggingT) output(s severity, buf *buffer, file string, line int, alsoTo
 		switch s {
 		case fatalLog:
 			l.file[fatalLog].Write(data)
-			fallthrough
 		case errorLog:
 			l.file[errorLog].Write(data)
-			fallthrough
 		case warningLog:
 			l.file[warningLog].Write(data)
-			fallthrough
 		case infoLog:
 			l.file[infoLog].Write(data)
 		}
